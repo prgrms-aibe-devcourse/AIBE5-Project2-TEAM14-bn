bed5629791ad13867aeaad5bca9059b2d27a80a2
aiegoo <eozz21@gmail.com>
2026-03-05 01:59:10 +0900

    feat: add MemberRepository layer

---
+++ src/main/java/com/aiegoo/comicrental/MemberRepository.java
@@ -0,0 +1,19 @@
+package com.aiegoo.comicrental;
+
+import com.aiegoo.comicrental.dao.MemberDAO;
+import com.aiegoo.comicrental.dao.MemberDAOImpl;
+
+import java.util.List;
+
+public class MemberRepository {
+    private final MemberDAO dao = new MemberDAOImpl();
+
+    public Member addMember(Member member) throws Exception {
+        dao.add(member);
+        return member;
+    }
+
+    public List<Member> listMembers() throws Exception {
+        return dao.listAll();
+    }
+}
